ce9929f24046c0f00d337c9d09788576d0a527cb
Andres <apalomino@edgebrain.io>
2018-12-18 19:18:45 +0100

    Initial package version and description of er_localisation to reproduce bag files and debug results

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # earth_rover_localisation
 
 Earth Rover localisation packages
+
+This package implements the localisation for the earth rover robot. 
+
+## Dependencies
+To succesfully compile this package you need:
+
+Real time implementation
+[robot_localization](http://docs.ros.org/kinetic/api/robot_localization/html/index.html): Robot Localization is a collection of state estimation nodes, each of which is an implementation of a nonlinear state estimator for robots moving in 3D space. It contains two state estimation nodes, ekf_localization_node and ukf_localization_node. In addition, robot_localization provides navsat_transform_node, which aids in the integration of GPS data.
+[Piksy Driver](https://support.swiftnav.com/customer/en/portal/articles/2924342-using-ros-with-swift-navigation-gnss-devices): Driver from ETH Zurich compatible with the Swift Navigation devices 
+[xsense Driver](http://wiki.ros.org/xsens_driver): Driver for the third and fourth generation of Xsens IMU devices
+
+PC host to monitorize results on GUI
+[Mapviz](https://github.com/swri-robotics/mapviz): Visualization tool with a plug-in system similar to RVIZ focused on visualizing 2D data.
+
+## Installation
+
+Create a workspace, clone the repository and compile it:
+	$ mkdir -p ~/er_ws/src  &&	cd ~/er_ws/src 	
+	$ git clone https://github.com/earthrover/earth_rover_localisation.git
+	$ cd .. & catkin_make
+	$ source devel/setup.bash
+
+## Use this package
+
+The package includes .bag example files of recorded tracks to run robot localization and tune the EKF params if necessary
+The following launch file reproduces a bag file and applies the robot localization to produce the pose estimation of the rover in a map
+
+	$ roslaunch er_localisation er_localisation.launch
+
+### Input
+
+The main topics published by the bag player to use on robot localization are:
+- /mti/sensor/imu: A [sensor_msgs/Imu.msg](http://docs.ros.org/melodic/api/sensor_msgs/html/msg/Imu.html) with the imu data
+- /heading: A [sensor_msgs/Imu.msg](http://docs.ros.org/melodic/api/sensor_msgs/html/msg/Imu.html) which reports the vehicle heading reported by the baseline between reference and attitude receiver
+- /piksi_receiver/navsatfix_best_fix: A [sensor_msgs/NavSatFix.msg](http://docs.ros.org/melodic/api/sensor_msgs/html/msg/NavSatFix.html) that contains WGS 84 coordinates with best available fix at the moment (either RTK or SBAS)
+
+### Nodes
+Three nodes are used on the architecture
+- heading_listener: In charge of traducing the vehicle heading from the baseline into ROS-compliant message to use in robot localization node
+- navsat_transform: Takes as input the GPS data and produces an odometry message in coordinates that are consistent with the robot’s world frame. More info on http://docs.ros.org/jade/api/robot_localization/html/navsat_transform_node.html.
+- ekf_localization: Robot localization node.. 
+
+### Outputs
+
+[nav_msgs/Odometry.msg](http://docs.ros.org/melodic/api/nav_msgs/html/msg/Odometry.html) 
---
+++ er_localisation/src/heading_listener.cpp
@@ -0,0 +1,86 @@
+/**
+ * @file heading_listener.cpp
+ * @brief This file includes a listener ROS node for the swiftnav heading message and publishes the ROS-compliant heading in a imu message.
+ * @author Andres Palomino (apalomino@edgebrain.io)
+ */
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "piksi_rtk_msgs/BaselineHeading.h"
+#include "sensor_msgs/Imu.h"
+#include <std_msgs/UInt32.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float64.h>
+#include <tf/transform_datatypes.h>
+
+# define M_PI           3.14159265358979323846  /* pi */
+# define sensor_way_deviation            0.002882263
+
+// Initialize variables
+sensor_msgs::Imu heading_rtk;
+std_msgs::Float32 heading_radians;
+
+/**
+* @brief Callback function to transform heading from RTK into ROS-compliant Imu message
+* @param Piksy baselineheading message
+*
+******************************************************************/
+void heading_callback(const piksi_rtk_msgs::BaselineHeading::ConstPtr& msg)
+{
+    std_msgs::UInt32 heading;
+    std_msgs::Float32 heading_deg;
+    // Get heading data (standard heading [mdeg])
+    heading.data =  msg->heading;
+    // Transform to ENU
+    heading_deg.data = (float)heading.data/1000;
+    heading_deg.data = 360 - heading_deg.data;    // Orientation increasing Counter clockwise
+    heading_deg.data = heading_deg.data + 90;    // 0° pointing East
+    if (heading_deg.data > 360.0)
+        heading_deg.data = heading_deg.data - 360.0;   
+    heading_radians.data = heading_deg.data * M_PI/180;
+
+    // Debug heading readings
+    ROS_DEBUG("Heading : [%d] deg ENU: [%f] rad ENU: [%f]", heading.data, heading_deg.data, heading_radians.data);
+
+    //Create imu msg from heading RTK
+    heading_rtk.header.stamp = ros::Time::now();
+    heading_rtk.header.frame_id = "gps_attitude";
+    heading_rtk.orientation = tf::createQuaternionMsgFromYaw(heading_radians.data);  // Create this quaternion from yaw (in radians)
+    // Calculated standard deviation for the sensor
+    double standard_deviation = sensor_way_deviation  * M_PI/180;
+    heading_rtk.orientation_covariance[8] = standard_deviation*standard_deviation;   //Update sensor covariance about z axe
+    heading_rtk.linear_acceleration.x = 0;
+    heading_rtk.linear_acceleration.y = 0;
+    heading_rtk.linear_acceleration.z = 0;
+    heading_rtk.linear_acceleration_covariance[0] = -1;
+    heading_rtk.angular_velocity.x = 0;
+    heading_rtk.angular_velocity.y = 0;
+    heading_rtk.angular_velocity.z = 0;
+    heading_rtk.angular_velocity_covariance[0] = -1;
+}
+
+/**
+* @brief Node to suscribe and publish heading results
+*
+******************************************************************/
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "heading_listener");
+    ros::NodeHandle n;
+
+    // Susbribers
+    ros::Subscriber sub_heading = n.subscribe("/piksi_attitude/baseline_heading", 1000, heading_callback);
+    // Publishers
+    ros::Publisher pub_heading = n.advertise<sensor_msgs::Imu>("/heading", 1);
+
+    ros::Rate loop_rate(30);
+    while (ros::ok())
+    {
+        pub_heading.publish(heading_rtk);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+  return 0;
+}
+
