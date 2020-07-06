fd014d12bdcd3cec8766598d4bc57419b31a5e0a
Ubuntu <ubuntu@ip-172-31-38-10.eu-west-2.compute.internal>
2020-07-06 09:32:28 +0000

    Finish first rtk

--- earth_rover_localization/scripts/get_first_RTK.py
+++ earth_rover_localization/scripts/get_first_RTK.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 ################################################################
 # Copyright (C) 2019 Earth Rover Limited. All rights reserved. #
@@ -12,34 +12,27 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 
 class FirstRTK():
-	def __init__(self, opath):
-		self.opath = opath
-		sub = rospy.Subscriber('/piksi_receiver/navsatfix_best_fix', NavSatFix, callback)
+    def __init__(self, opath):
+        self.opath = opath
+        # Subscribe once to RTK topic and get first msg
+        msg = rospy.wait_for_message('/piksi_receiver/navsatfix_best_fix', NavSatFix, timeout=None)
+        
+        # Write GPS corrdinates to yaml cfg file
+        text_file = open(self.opath+"/enu_origin.yaml", "w")
+        text_file.write("latitude0_deg: " + str(msg.latitude) + '\r\n')
+        text_file.close()
 
-	def callback(self, msg):
-		text_file = open(self.opath/+"enu_origin.yaml", "w")
-		text_file.write("latitude0_deg: " + msg.latitude)
-		text_file.close()
-
-		text_file = open(self.opath/+"enu_origin.yaml", "a")
-		text_file.write("longitude0_deg: " + msg.longitude)
-		text_file.write("altitude0: " + msg.altitude)
-		text_file.close()
-
-# While loop
-def main(self):
-	try:
-		rospy.spin()
-	except (rospy.ROSInterruptException):
-		pass
+        text_file = open(self.opath+"/enu_origin.yaml", "a")
+        text_file.write("longitude0_deg: " + str(msg.longitude) + '\r\n')
+        text_file.write("altitude0: " + str(msg.altitude) + '\r\n')
+        text_file.close()
 
 if __name__ == "__main__":
-	# get passed arguments:
+    
+    # get passed arguments:
     input_args = rospy.myargv(argv=sys.argv)
-	output_path = input_args[1]
+    output_path = input_args[1]
 
-	# init ros node
+    # init ros node
     rospy.init_node('first_RTK')
-	listen_rtk = FirstRTK(opath=output_path)
-
-	listen_rtk.main()
+    listen_rtk = FirstRTK(opath=output_path)
