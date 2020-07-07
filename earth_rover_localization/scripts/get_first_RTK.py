78b592525975ac74d00ead9afdccee8bdd5de139
mcalafat-earthrover <mateu.calafat@earthrover.farm>
2020-07-07 12:17:21 +0200

    Prepare for mapviz

--- earth_rover_localization/scripts/get_first_RTK.py
+++ earth_rover_localization/scripts/get_first_RTK.py
@@ -12,34 +12,36 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 
 class FirstRTK():
-	def __init__(self, opath):
-		self.opath = opath
-		sub = rospy.Subscriber('/piksi_receiver/navsatfix_best_fix', NavSatFix, callback)
 
-	def callback(self, msg):
-		text_file = open(self.opath/+"enu_origin.yaml", "w")
-		text_file.write("latitude0_deg: " + msg.latitude)
-		text_file.close()
+    def __init__(self, opath):
+        self.opath = opath
+        sub = rospy.Subscriber('/piksi_receiver/navsatfix_best_fix', NavSatFix, callback)
 
-		text_file = open(self.opath/+"enu_origin.yaml", "a")
-		text_file.write("longitude0_deg: " + msg.longitude)
-		text_file.write("altitude0: " + msg.altitude)
-		text_file.close()
+    def callback(self, msg):
+        text_file = open(self.opath/+"enu_origin.yaml", "w")
+        text_file.write("latitude0_deg: " + msg.latitude)
+        text_file.close()
 
-# While loop
-def main(self):
-	try:
-		rospy.spin()
-	except (rospy.ROSInterruptException):
-		pass
+        text_file = open(self.opath/+"enu_origin.yaml", "a")
+        text_file.write("longitude0_deg: " + msg.longitude)
+        text_file.write("altitude0: " + msg.altitude)
+        text_file.close()
+
+    # While loop
+    def main(self):
+        try:
+            rospy.spin()
+        except (rospy.ROSInterruptException):
+            pass
 
 if __name__ == "__main__":
-	# get passed arguments:
+
+    # get passed arguments:
     input_args = rospy.myargv(argv=sys.argv)
-	output_path = input_args[1]
+    output_path = input_args[1]
 
-	# init ros node
+    # init ros node
     rospy.init_node('first_RTK')
-	listen_rtk = FirstRTK(opath=output_path)
+    listen_rtk = FirstRTK(opath=output_path)
 
-	listen_rtk.main()
+    listen_rtk.main()
