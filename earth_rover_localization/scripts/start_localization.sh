c1668a7d8f4b9a6562f2433c6d0be35c4d0f7f36
tpieras-earthrover <tomas.pieras@earthrover.farm>
2020-09-15 17:00:01 +0200

    Corrected minor discrepancies

--- earth_rover_localization/scripts/start_localization.sh
+++ earth_rover_localization/scripts/start_localization.sh
@@ -7,6 +7,9 @@ rosrun earth_rover_localization get_first_RTK.py $(rospack find earth_rover_loca
 if [ ${mount} == "tractor" ]
 then
    roslaunch earth_rover_localization scouting_localization_node.launch
+elif [ ${mount} == "rover" ]
+then
+   roslaunch earth_rover_localization rover_localization_node.launch 
 else
-   roslaunch earth_rover_localization rover_localization_node.launch
+   echo "ERROR: ${mount} is not an acceptable argument"
 fi
