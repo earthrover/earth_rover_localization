ce3bfeb9636ff4b52c724457915944d01bb0baf2
tpieras-earthrover <tomas.pieras@earthrover.farm>
2020-09-15 12:53:14 +0200

    Added mounting type argument

--- earth_rover_localization/scripts/start_localization.sh
+++ earth_rover_localization/scripts/start_localization.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+mount=$1
+
 rosrun earth_rover_localization get_first_RTK.py $(rospack find earth_rover_localization)/cfg
-roslaunch earth_rover_localization start_localization_node.launch
+
+if [ ${mount} == "tractor" ]
+then
+   roslaunch earth_rover_localization scouting_localization_node.launch
+else
+   roslaunch earth_rover_localization rover_localization_node.launch
+fi
